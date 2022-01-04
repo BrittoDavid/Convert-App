e87ece02f6a3151050da53a132df5b30cc0a6eb3
David Britto <brittoargote789@gmail.com>
2022-01-03 23:22:05 -0500

    Finished the TS file, working on the convert method

--- src/app/components/convert/convert.component.ts
+++ src/app/components/convert/convert.component.ts
@@ -7,6 +7,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvertComponent implements OnInit {
 
+  cantidad = 0;
+  tengo = 'USD';
+  quiero = 'EUR';
+  total = 0;
+
+  monedas: string[] = ['USD','EUR','LIBRA','PESOS'];
+
+  convertir(): void
+  {
+    switch(this.tengo)
+    {
+      case 'USD':
+        if( this.quiero === 'USD')
+        {
+          this.total = this.cantidad
+        }else if(this.quiero === 'LIBRA')
+        {
+          this.total = this.cantidad * 2 
+        }else if(this.quiero === 'EUR')
+        {
+          this.total = this.cantidad * 3 
+        }else if(this.quiero === 'PESOS')
+        {
+          this.total = this.cantidad * 4 
+        }
+      break;
+      case 'EUR':
+        if( this.quiero === 'USD')
+        {
+          this.total = this.cantidad
+        }else if(this.quiero === 'LIBRA')
+        {
+          this.total = this.cantidad * 2 
+        }else if(this.quiero === 'EUR')
+        {
+          this.total = this.cantidad * 3 
+        }else if(this.quiero === 'PESOS')
+        {
+          this.total = this.cantidad * 4 
+        }
+      break;
+      case 'LIBRA':
+        if( this.quiero === 'USD')
+        {
+          this.total = this.cantidad
+        }else if(this.quiero === 'LIBRA')
+        {
+          this.total = this.cantidad * 2 
+        }else if(this.quiero === 'EUR')
+        {
+          this.total = this.cantidad * 3 
+        }else if(this.quiero === 'PESOS')
+        {
+          this.total = this.cantidad * 4 
+        }
+      break;
+      case 'PESOS':
+        if( this.quiero === 'USD')
+        {
+          this.total = this.cantidad
+        }else if(this.quiero === 'LIBRA')
+        {
+          this.total = this.cantidad * 2 
+        }else if(this.quiero === 'EUR')
+        {
+          this.total = this.cantidad * 3 
+        }else if(this.quiero === 'PESOS')
+        {
+          this.total = this.cantidad * 4 
+        }
+      break;
+      default:
+      alert("Muy chistoso, no?");
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
